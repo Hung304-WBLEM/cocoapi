4310f4a9a564bbf1fffbd060616e35cfcd5b3a20
Hung Vo <hqvo2@maui.rcdc.uh.edu>
2021-01-19 03:17:16 -0600

    Update matlab eval

--- MatlabAPI/evalDemo.m
+++ MatlabAPI/evalDemo.m
@@ -1,24 +1,27 @@
 %% Demo demonstrating the algorithm result formats for COCO
 
 %% select results type for demo (either bbox or segm)
-type = {'segm','bbox','keypoints'}; type = type{1}; % specify type here
+type = {'segm','bbox','keypoints'}; type = type{2}; % specify type here
 fprintf('Running demo for *%s* results.\n\n',type);
 
 %% initialize COCO ground truth api
 dataDir='../'; prefix='instances'; dataType='val2014';
 if(strcmp(type,'keypoints')), prefix='person_keypoints'; end
-annFile=sprintf('%s/annotations/%s_%s.json',dataDir,prefix,dataType);
+% annFile=sprintf('%s/annotations/%s_%s.json',dataDir,prefix,dataType);
+annFile='/home/hqvo2/Projects/Breast_Cancer/data/processed_data/mass/test/annotation_coco_with_classes.json'
 cocoGt=CocoApi(annFile);
 
 %% initialize COCO detections api
-resFile='%s/results/%s_%s_fake%s100_results.json';
-resFile=sprintf(resFile,dataDir,prefix,dataType,type);
+% resFile='%s/results/%s_%s_fake%s100_results.json';
+resFile='/home/hqvo2/Projects/Breast_Cancer/libs/mmdetection/result.bbox.json'
+% resFile=sprintf(resFile,dataDir,prefix,dataType,type);
 cocoDt=cocoGt.loadRes(resFile);
 
 %% visialuze gt and dt side by side
 imgIds=sort(cocoGt.getImgIds()); imgIds=imgIds(1:100);
 imgId = imgIds(randi(100)); img = cocoGt.loadImgs(imgId);
-I = imread(sprintf('%s/images/val2014/%s',dataDir,img.file_name));
+% I = imread(sprintf('%s/images/val2014/%s',dataDir,img.file_name));
+I = imread(sprintf('/home/hqvo2/Datasets/processed_data/mass/test/%s',img.file_name));
 figure(1); subplot(1,2,1); imagesc(I); axis('image'); axis off;
 annIds = cocoGt.getAnnIds('imgIds',imgId); title('ground truth')
 anns = cocoGt.loadAnns(annIds); cocoGt.showAnns(anns);
